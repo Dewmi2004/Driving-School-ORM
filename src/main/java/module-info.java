c96d8c8541757c23d86aed14993844ff3bfb7d02
Dewmi2004 <imashadewmi557@gmail.com>
2025-09-04 17:12:05 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module lk.ijse.drivingschoolorm {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens lk.ijse.drivingschoolorm to javafx.fxml;
+    exports lk.ijse.drivingschoolorm;
+}
